755fa03458103e6a02ac56f1fa96618a0364ab09
Martina9771 <martina.karnikova@email.cz>
2026-02-13 10:47:45 +0100

    13.02.

---
+++ 3. lekce 13.02/script.js
@@ -0,0 +1,19 @@
+alert("ahoj")
+
+//proměné pro jména 
+userName = "Pepíček";
+console.log(userName); 
+
+userName = "Mařenka"; 
+console.log(userName);
+
+//proměnné pro věk obou lidí
+console.log(16+17);
+let vek1 = 16;
+let vek2 = 17;
+
+//součet věků do nové proměnné
+let soucet = vek1 + vek2;
+
+//výpis věty do konzole
+console.log(userName + " a " + userName + "mají dohromady" + soucet + " let. ")
